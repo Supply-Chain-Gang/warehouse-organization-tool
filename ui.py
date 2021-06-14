f9e8632704065fffa5212c743e8dc194c144ebb4
SkylerJohnson102020 <basseuph@gmail.com>
2021-06-14 11:33:12 -0700

    ui.py updateinformation class added.

--- ui.py
+++ ui.py
@@ -3,12 +3,24 @@ import numpy as np
 from warehouse_organization_tool.utilities.user_input import UserInput
 from warehouse_organization_tool.terminal_ui_processes.ui_processes import Menu
 
+class UpdateInformation:
+  def __init__(self):
+    self.item_for_update = {}
+
 def main():
   menu = Menu()
+  update_info = UpdateInformation()
   while True:
     csv_count_update = menu.get_initial_user_decisions()
+    # print(csv_count_update)
     if csv_count_update:
-      print(csv_count_update)
+      for item in csv_count_update:
+        if item in update_info.item_for_update:
+          update_info.item_for_update[item] += csv_count_update[item]
+          continue
+        update_info.item_for_update[item] = csv_count_update[item]
+    print(csv_count_update)
+    print(update_info.item_for_update)
       #do csv update stuff here
 
 if __name__ == '__main__':
