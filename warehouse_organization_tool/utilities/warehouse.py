a89937b73d04ac969ea995dfd293514fb3c63491
Zach Hornung <Zachhornung@comcast.net>
2021-06-14 17:12:51 -0700

    updated warehouse and skylers ascii art

--- warehouse_organization_tool/utilities/warehouse.py
+++ warehouse_organization_tool/utilities/warehouse.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 class Warehouse:
   #all dimensions in inches
-  def __init__(self, length=50, width=50, height=50, **kwargs):
+  def __init__(self, length=50, width=50, height=10, **kwargs):
     self.length = length*12
     self.width = width*12
     self.height = height*12
@@ -13,7 +13,7 @@ class Warehouse:
     self.x_grid_space = 0
     self.y_grid_space = 0
     self.z_grid_space = 0
-    self.grid = np.zeros((self.x_grid_space,self.y_grid_space+1,self.z_grid_space))
+    self.grid = None
 
     if 'lane_size' in kwargs:
       self.lane_width_size = kwargs['lane_size']*12
@@ -23,15 +23,18 @@ class Warehouse:
     num_shelves_back_wall = (self.width//shelf.length)*(self.height//shelf.height)
     x_shelves = (self.width - self.lane_width_size)//(shelf.length)
     y_shelves = (self.length - shelf.depth - self.lane_width_size)//(shelf.depth + self.lane_width_size)
-    z_shelves = (x_shelves + y_shelves + num_shelves_back_wall)*(self.height//shelf.height)
+    # z_shelves = (x_shelves + y_shelves + num_shelves_back_wall)*(self.height//shelf.height)
+    z_shelves = (self.height//shelf.height)
     self.x_grid_space,self.y_grid_space,self.z_grid_space = x_shelves,y_shelves,z_shelves
     return x_shelves,y_shelves,z_shelves,num_shelves_back_wall
 
   # TODO
   def place_shelves(self):
     x,y,z,backwall = self.calculate_num_of_shelves()
-    print("Warehouse Grid",self.grid)
+    self.grid = np.zeros((self.x_grid_space,self.y_grid_space+1,self.z_grid_space))
     print(x,y,z,backwall)
+    print(self.grid)
+    
     # Need a dictionary of the total num of items, total quantity of each item, and the total sales volume of each item
 
 class Shelves:
@@ -40,4 +43,3 @@ class Shelves:
     self.depth = depth*12
     self.height = height*12
     self.footprint = self.length*self.depth
-      
